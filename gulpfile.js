7c18e9591d5a1a6be5ad6509e169d94df91d6774
hots0uce <jm.giuffrida@gmail.com>
2014-12-28 21:47:07 -0500

    Autoprefixing, minfication of less, notifications

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var browserify = require('gulp-browserify');
 var uglify = require('gulp-uglify');
 var less = require('gulp-less');
 var path = require('path');
+var minifyCSS = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var notify = require('gulp-notify');
 
 gulp.task('scripts', function() {
   // Minify and copy all JavaScript (except vendor scripts)
@@ -13,7 +16,8 @@ gulp.task('scripts', function() {
           debug : !gulp.env.production
         }))
         .pipe(uglify())
-    .pipe(gulp.dest('js/dist'));
+    .pipe(gulp.dest('js/dist'))
+    .pipe(notify('Scripts browserify\'ed and uglified'));
 });
 
 gulp.task('css',function() {
@@ -22,8 +26,14 @@ gulp.task('css',function() {
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(gulp.dest('./css'));
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./css'))
+    .pipe(notify('Less compiled, prefixed, minified'));
+});
 
+gulp.task('watchall',function() {
+	gulp.watch(['js/src/**/*.js','css/less/**/*.less'], ['scripts','css']);
 });
 
 
