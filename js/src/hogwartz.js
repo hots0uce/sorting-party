1fc10271155ff16e35a0d1e3044f99118460f360
hots0uce <jm.giuffrida@gmail.com>
2014-12-31 16:14:48 -0500

    +better

--- js/src/hogwartz.js
+++ js/src/hogwartz.js
@@ -2,7 +2,7 @@ var TeamSorter = require('./team.js'),
 	audio = require('./audio.js'),
 	houses = ['Gryffindor','Hufflepuff','Ravenclaw','Slytherin'],
 	ts,
-	soundFIles = {
+	soundFiles = {
 		'Gryffindor': '/sorting-party/audio/gr.mp3',
 		'Hufflepuff': '/sorting-party/audio/h.mp3',
 		'Ravenclaw': '/sorting-party/audio/r.mp3',
@@ -15,15 +15,15 @@ exports.init = function(playersCount) {
 	exports.ts = new TeamSorter(houses.length, playersCount);
 	this.remaining = this.ts.availableTeams.length;
 
-	audio.loadSound(soundFile,function() {
-
-	});
+	for(var i=0; i<houses.length; i++) {
+		audio.loadSound(soundFiles[houses[i]],function() {});	
+	}
 };
 
 exports.pickTeam = function() {
 	var teamIdx = exports.ts.pickTeam();
 	this.remaining = this.ts.availableTeams.length;
-	audio.playSound(soundFilef[houses[teamIdx]]);
+	audio.playSound(soundFiles[houses[teamIdx]]);
 	return houses[teamIdx];
 };
 
